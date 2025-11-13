376490c4f9394bb2994904a63364ffc2a44905c9
Louis Davis <79977953+louiswdavis@users.noreply.github.com>
2025-11-13 22:31:19 +0000

    schema and annotations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,19 +2,28 @@
 #
 # Table name: tasks
 #
-#  id          :bigint           not null, primary key
-#  description :text
-#  due_date    :datetime
-#  priority    :integer          default("medium"), not null
-#  status      :integer          default("todo"), not null
-#  title       :string           not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  campaign_id :bigint
+#  id             :bigint           not null, primary key
+#  description    :text
+#  due_date       :datetime
+#  priority       :integer          default("medium"), not null
+#  status         :integer          default("todo"), not null
+#  title          :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  assigned_to_id :bigint
+#  campaign_id    :bigint
+#  created_by_id  :bigint
 #
 # Indexes
 #
-#  index_tasks_on_campaign_id  (campaign_id)
+#  index_tasks_on_assigned_to_id  (assigned_to_id)
+#  index_tasks_on_campaign_id     (campaign_id)
+#  index_tasks_on_created_by_id   (created_by_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (assigned_to_id => users.id)
+#  fk_rails_...  (created_by_id => users.id)
 #
 class Task < ApplicationRecord
   belongs_to :campaign, counter_cache: :task_count # counter_cache could also just be a method but this is my familiar approach
