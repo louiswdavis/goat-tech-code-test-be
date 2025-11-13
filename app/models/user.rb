73e16ccf5f2621fa47152bfe9e0d265695effa41
Louis Davis <79977953+louiswdavis@users.noreply.github.com>
2025-11-13 22:30:33 +0000

    Add user associations to tasks
    Uncommented user-task associations in the User model and added a migration to link tasks to their creators and assignees via foreign keys. This enables tracking which user created or is assigned to each task.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,6 @@ class User < ApplicationRecord
 
   # Aliased associations for tasks (when Task is created by candidate)
   # BONUS TASK:
-  # has_many :created_tasks, class_name: 'Task', foreign_key: 'created_by_id', dependent: :nullify
-  # has_many :assigned_tasks, class_name: 'Task', foreign_key: 'assigned_to_id', dependent: :nullify
+  has_many :created_tasks, class_name: 'Task', foreign_key: 'created_by_id', dependent: :nullify
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: 'assigned_to_id', dependent: :nullify
 end
